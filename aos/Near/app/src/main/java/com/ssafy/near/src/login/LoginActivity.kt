c6aca32e56f38ee3cec5c8c3f08761526782dfdd
KimDoyeon <vlxjdus@gmail.com>
2022-01-18 11:54:38 +0900

    [S06P12D203-25] [AOS - 김도연] feature: login 페이지 생성 & UI 구현

---
+++ aos/Near/app/src/main/java/com/ssafy/near/src/login/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.ssafy.near.src.login
+
+import android.os.Bundle
+import com.ssafy.near.R
+import com.ssafy.near.config.BaseActivity
+import com.ssafy.near.databinding.ActivityLoginBinding
+
+class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
