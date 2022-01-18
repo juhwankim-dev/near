9d3af7e8ed18f91a6e1f4267192dd69627f065e4
KimDoyeon <vlxjdus@gmail.com>
2022-01-18 20:58:33 +0900

    [S06P12D203-23] [AOS - 김도연] feat: 회원가입 UI 구현

---
+++ aos/Near/app/src/main/java/com/ssafy/near/src/signup/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.ssafy.near.src.signup
+
+import android.os.Bundle
+import com.ssafy.near.R
+import com.ssafy.near.config.BaseActivity
+import com.ssafy.near.databinding.ActivitySignUpBinding
+
+class SignUpActivity : BaseActivity<ActivitySignUpBinding>(R.layout.activity_sign_up) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
