a0365db1e239c5aa6c21b5c349e9e6c396890606
KimDoyeon <vlxjdus@gmail.com>
2022-01-18 21:40:37 +0900

    [S06P12D203-54] [AOS - 김도연] feat: 회원 정보 수정 UI 구현

---
+++ aos/Near/app/src/main/java/com/ssafy/near/src/edituserinfo/EditUserInfoActivity.kt
@@ -0,0 +1,13 @@
+package com.ssafy.near.src.edituserinfo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ssafy.near.R
+import com.ssafy.near.config.BaseActivity
+import com.ssafy.near.databinding.ActivityEditUserInfoBinding
+
+class EditUserInfoActivity : BaseActivity<ActivityEditUserInfoBinding>(R.layout.activity_edit_user_info) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
