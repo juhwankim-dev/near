8d45f9440a16fb3131547178967611e909108e80
Juhwan Kim <juhwan.dev@gmail.com>
2022-01-18 11:10:56 +0900

    [S06P12D203-67] [AOS-김주환] feat: 대문페이지 및 navigation 틀 생성

--- aos/Near/app/src/main/java/com/ssafy/near/src/MainActivity.kt
+++ aos/Near/app/src/main/java/com/ssafy/near/src/MainActivity.kt
@@ -2,6 +2,8 @@ package com.ssafy.near.src
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.ssafy.near.R
 import com.ssafy.near.config.BaseActivity
 import com.ssafy.near.databinding.ActivityMainBinding
@@ -10,5 +12,14 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 네비게이션 호스트
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+
+        // 네비게이션 컨트롤러
+        val navController = navHostFragment.navController
+
+        // 바인딩
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
     }
 }
---
+++ aos/Near/app/src/main/java/com/ssafy/near/src/main/home/HomeFragment.kt
@@ -0,0 +1,14 @@
+package com.ssafy.near.src.main.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ssafy.near.R
+import com.ssafy.near.config.BaseFragment
+import com.ssafy.near.databinding.FragmentHomeBinding
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+
+}
