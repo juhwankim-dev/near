17338b39cc5bb26dd13fef79b6b1acb6a516f7fa
Juhwan Kim <juhwan.dev@gmail.com>
2022-01-19 13:28:21 +0900

    [S06P12D203-51] [AOS-김주환] feat: 수어 목록 페이지 radiobutton 및 recyclerview 작성

---
+++ aos/Near/app/src/main/java/com/ssafy/near/src/main/handsign/HandSignAdapter.kt
@@ -0,0 +1,49 @@
+package com.ssafy.near.src.main.handsign
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ssafy.near.databinding.ListItemHandSignBinding
+
+class HandSignAdapter : RecyclerView.Adapter<HandSignAdapter.HandSignViewHolder>() {
+    private lateinit var itemClickListner: ItemClickListener
+    var list = mutableListOf<String>()
+
+    inner class HandSignViewHolder(private val binding: ListItemHandSignBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bindInfo(s: String) {
+            binding.voca = s
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HandSignViewHolder {
+        var listItemBinding = ListItemHandSignBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return HandSignViewHolder(listItemBinding)
+    }
+
+    override fun onBindViewHolder(holder: HandSignViewHolder, position: Int) {
+        holder.apply {
+            bindInfo(list[position])
+
+            itemView.setOnClickListener {
+                //itemClickListner.onClick(list[position].o_id) // TODO 나중에 BE 다 만들어지면 수정
+            }
+        }
+    }
+
+    // TODO 나중에 BE 다 만들어지면 수정
+    fun updateList() {
+
+    }
+
+    override fun getItemCount(): Int = list.size
+
+    //클릭 인터페이스 정의 사용하는 곳에서 만들어준다.
+    interface ItemClickListener {
+        fun onClick(id: Int)
+    }
+
+    //클릭리스너 등록 매소드
+    fun setItemClickListener(itemClickListener: ItemClickListener) {
+        this.itemClickListner = itemClickListener
+    }
+}
--- aos/Near/app/src/main/java/com/ssafy/near/src/main/handsign/HandSignFragment.kt
+++ aos/Near/app/src/main/java/com/ssafy/near/src/main/handsign/HandSignFragment.kt
@@ -1,9 +1,35 @@
 package com.ssafy.near.src.main.handsign
 
+import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ssafy.near.R
 import com.ssafy.near.config.BaseFragment
 import com.ssafy.near.databinding.FragmentHandSignBinding
 
 class HandSignFragment : BaseFragment<FragmentHandSignBinding>(R.layout.fragment_hand_sign) {
+    lateinit var handSignAdapter: HandSignAdapter
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initView()
+        initEvent()
+    }
+
+    private fun initView() {
+        handSignAdapter = HandSignAdapter()
+        binding.rvHandSign.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = handSignAdapter
+        }
+    }
+
+    private fun initEvent() {
+        handSignAdapter.setItemClickListener(object : HandSignAdapter.ItemClickListener{
+            override fun onClick(o_id: Int) {
+                // TODO 단어가 클릭되었을 때 할 동작
+            }
+        })
+    }
 }
