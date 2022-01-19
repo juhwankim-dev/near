1efba034cdd2360bc3073c6075d173efa1abeacd
Juhwan Kim <juhwan.dev@gmail.com>
2022-01-19 11:15:52 +0900

    [S06P12D203-66] [AOS-김주환] feat: 회원 정보 수정, 설정, 내 단어장 메뉴 추가

--- aos/Near/app/src/main/java/com/ssafy/near/src/main/mypage/MyPageFragment.kt
+++ aos/Near/app/src/main/java/com/ssafy/near/src/main/mypage/MyPageFragment.kt
@@ -1,14 +1,34 @@
 package com.ssafy.near.src.main.mypage
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.ssafy.near.R
 import com.ssafy.near.config.BaseFragment
 import com.ssafy.near.databinding.FragmentMyPageBinding
 
 class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        initView()
+        initEvent()
+    }
+
+    private fun initView() {
+
+    }
+
+    private fun initEvent() {
+        binding.tvMemberModify.setOnClickListener {
+
+        }
+
+        binding.tvSetting.setOnClickListener {
+
+        }
+
+        binding.tvMyNote.setOnClickListener {
+
+        }
+    }
 }
