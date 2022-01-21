6325beae09c60b9edd798f5c2c4f67fccda43ee6
KimDoyeon <vlxjdus@gmail.com>
2022-01-21 18:17:19 +0900

    [S06P12D203-88] [AOS-김도연] feat: 수정된 정보 중복 검사, 바꾸지 않았다면 검사하지 않는다. 아직 테스트 안 함.

--- aos/Near/app/src/main/java/com/ssafy/near/src/edituserinfo/EditUserInfoActivity.kt
+++ aos/Near/app/src/main/java/com/ssafy/near/src/edituserinfo/EditUserInfoActivity.kt
@@ -3,6 +3,7 @@ package com.ssafy.near.src.edituserinfo
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.ViewModelProvider
 import com.ssafy.near.R
 import com.ssafy.near.config.ApplicationClass
@@ -13,9 +14,15 @@ import com.ssafy.near.repository.UserRepository
 import com.ssafy.near.src.UserViewModel
 import com.ssafy.near.src.UserViewModelFactory
 import com.ssafy.near.src.login.LoginActivity
+import com.ssafy.near.util.Validation
 
-class EditUserInfoActivity : BaseActivity<ActivityEditUserInfoBinding>(R.layout.activity_edit_user_info) {
+class EditUserInfoActivity :
+    BaseActivity<ActivityEditUserInfoBinding>(R.layout.activity_edit_user_info) {
     lateinit var userViewModel: UserViewModel
+    var isCheckedNickname = false
+    var isCheckedEmail = false
+    var isCheckedPw = false
+    var isCheckedConfirmPw = false
     var oldInfo = UserInfo()
     var newInfo = UserInfo()
 
@@ -24,8 +31,8 @@ class EditUserInfoActivity : BaseActivity<ActivityEditUserInfoBinding>(R.layout.
 
         initViewModel()
         initData()
+        initValidation()
         initEvent()
-
     }
 
     private fun initViewModel() {
@@ -41,6 +48,50 @@ class EditUserInfoActivity : BaseActivity<ActivityEditUserInfoBinding>(R.layout.
                 oldInfo = it
             }
         })
+
+        userViewModel.getCheckedNickname().observe(this, {
+            if (it) {
+                if (binding.etNickname.editText?.text.toString() == oldInfo.nickname) {
+                    isCheckedNickname = true
+                    binding.etNickname.apply {
+                        error = ""
+                        helperText = ""
+                    }
+                } else {
+                    isCheckedNickname = false
+                    binding.etNickname.apply {
+                        error = "이미 존재하는 닉네임입니다."
+                        helperText = ""
+                    }
+                }
+            } else {
+                isCheckedNickname = true
+                binding.etNickname.apply {
+                    error = ""
+                    helperText = "사용 가능한 닉네임입니다."
+                }
+            }
+        })
+
+        userViewModel.getCheckedEmail().observe(this, {
+            if (it) {
+                if (binding.etEmail.editText?.text.toString() == oldInfo.email) {
+                    isCheckedEmail = true
+                    binding.etNickname.apply {
+                        error = ""
+                        helperText = ""
+                    }
+                } else {
+                    isCheckedEmail = false
+                    binding.etEmail.error = "이미 존재하는 이메일입니다."
+                    binding.etEmail.helperText = ""
+                }
+            } else {
+                isCheckedEmail = true
+                binding.etEmail.error = ""
+                binding.etEmail.helperText = "사용 가능한 이메일입니다."
+            }
+        })
     }
 
     private fun initData() {
@@ -52,10 +103,50 @@ class EditUserInfoActivity : BaseActivity<ActivityEditUserInfoBinding>(R.layout.
         }
     }
 
+    private fun initValidation() {
+        binding.etNickname.editText?.addTextChangedListener {
+            when {
+                Validation.validateNickname(it.toString(),
+                    binding.etNickname) -> checkDuplicatedNickname(it.toString())
+                else -> isCheckedNickname = false
+            }
+        }
+
+        binding.etEmail.editText?.addTextChangedListener {
+            when {
+                Validation.validateEmail(it.toString(),
+                    binding.etEmail) -> checkDuplicatedEmail(it.toString())
+                else -> isCheckedEmail = false
+            }
+        }
+
+        binding.etNewPw.editText?.addTextChangedListener {
+            isCheckedPw = Validation.validatePw(it.toString(), binding.etNewPw)
+            isCheckedConfirmPw =
+                Validation.confirmPw(binding.etConfirmNewPw.editText?.text.toString(),
+                    it.toString(),
+                    binding.etConfirmNewPw)
+        }
+
+        binding.etConfirmNewPw.editText?.addTextChangedListener {
+            isCheckedConfirmPw = Validation.confirmPw(it.toString(),
+                binding.etNewPw.editText?.text.toString(),
+                binding.etConfirmNewPw)
+        }
+    }
+
     private fun initEvent() {
         binding.btnSave.setOnClickListener {
 
         }
+    }
+
+    private fun checkDuplicatedNickname(nickname: String) {
+        userViewModel.checkDuplicatedNickname(nickname)
+    }
+
+    private fun checkDuplicatedEmail(email: String) {
+        userViewModel.checkDuplicatedEmail(email)
     }
 
     private fun updateUserInfo() {
