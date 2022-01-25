d23a14fa66060efc58ab39cf9b72048c6613a79d
Juhwan Kim <juhwan.dev@gmail.com>
2022-01-25 11:37:27 +0900

    [S06P12D203-18] [AOS-김주환] feat: ExoPlayer 적용

--- aos/Near/app/src/main/java/com/ssafy/near/src/main/handsign/HandSignDetailActivity.kt
+++ aos/Near/app/src/main/java/com/ssafy/near/src/main/handsign/HandSignDetailActivity.kt
@@ -1,12 +1,21 @@
 package com.ssafy.near.src.main.handsign
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.ssafy.near.R
 import com.ssafy.near.config.BaseActivity
 import com.ssafy.near.databinding.ActivityHandSignDetailBinding
 
 class HandSignDetailActivity : BaseActivity<ActivityHandSignDetailBinding>(R.layout.activity_hand_sign_detail)  {
+    private var videoPlayer: SimpleExoPlayer? = null
+    private var sampleUrl = "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -20,9 +29,43 @@ class HandSignDetailActivity : BaseActivity<ActivityHandSignDetailBinding>(R.lay
         } else {
             showToastMessage("서버로부터 응답받지 못했습니다")
         }
+
+        // ExoPlayer 인스턴스를 생성하고 소스를 플레이에 할당하여 비디오 플레이어 초기화
+        videoPlayer = SimpleExoPlayer.Builder(this).build()
+        binding.pvHandSign.player = videoPlayer
+        buildMediaSource()?.let {
+            videoPlayer?.prepare(it)
+        }
     }
 
     private fun initEvent() {
         // TODO LiveData를 통해 값을 받아와 view에게 넘겨주기
+    }
+
+    // MediaSource: 영상에 출력할 미디어 정보를 가져오는 클래스
+    private fun buildMediaSource(): MediaSource? {
+        val dataSourceFactory = DefaultDataSourceFactory(this, "sample")
+        return ProgressiveMediaSource.Factory(dataSourceFactory)
+            .createMediaSource(Uri.parse(sampleUrl))
+    }
+
+    // 일시중지
+    override fun onResume() {
+        super.onResume()
+        videoPlayer?.playWhenReady = true
+    }
+
+    // 정지
+    override fun onStop() {
+        super.onStop()
+        videoPlayer?.playWhenReady = false
+        if (isFinishing) {
+            releasePlayer()
+        }
+    }
+
+    // 종료
+    private fun releasePlayer() {
+        videoPlayer?.release()
     }
 }
