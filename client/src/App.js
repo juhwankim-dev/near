8a01584ef2b6b3147b6014afb54c5767406a34e2
wooyeongs2 <wooyeongs2@gmail.com>
2022-01-26 11:44:38 +0900

    [S06P12D203-63] [FE-고우영] feat: 수어 학습 페이지 생성 및 라우터 연결

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,8 @@ import LoginPage from './components/views/LoginPage/LoginPage';
 import RegisterPage from './components/views/RegisterPage/RegisterPage';
 import MainPage from './components/views/MainPage/MainPage';
 import NavBar from './components/views/NavBar/NavBar';
+import SignPage from './components/views/ContentPage/SignPage/SignPage';
+
 import Auth from './hoc/auth' //1.해당페이지에 들어올 수 있는 유저인지 확인
 
 
@@ -15,13 +17,14 @@ function App() {
   return ( 
     <Router>
     <NavBar>
+      </NavBar>   
       <Routes>
         <Route exact path="/" element={<LandingPage /> } />
         {/* <Route exact path="/" element={<LandingPage /> } /> */}
         <Route path="/main" element={<MainPage />} />
-        </Routes>
-      </NavBar>    
-        <Routes>
+        <Route exact path="/sign" element={<SignPage />} />
+ 
+
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         {/* <Route exact path="/" element={Auth(<LandingPage />, null) } />
@@ -29,7 +32,7 @@ function App() {
         <Route path="/register" element={Auth(<RegisterPage />)} />
         이거 Auth활용해서 페이지 제한 둬야하는데 당장에는 Auth연결을 백이랑 안해서 그런지 페이지 안떠서 일단 주석처리함 */}
       </Routes>
-      <MainPage></MainPage>
+      
     </Router>
   );
 } 
---
+++ client/src/components/views/ContentPage/SignPage/SignPage.js
@@ -0,0 +1,62 @@
+// 구성: SignPage/SignCard/SignCardDetail
+
+import React, { useState } from "react";
+import { Pagination } from 'react-bootstrap';
+import Data from './SignData.js';
+import SignCard from './SignCard.js';
+// import './SignPage.css';
+import './SignPage.scss';
+
+
+function SignPage(){
+  let [signcards, setsigncard] = useState(Data);
+  let [modal, setmodal] = useState(false);
+ 
+ 
+  return (
+    <div className="container">
+      <h1>수어 배우기</h1>
+      <div className='nemo'> 
+      
+      { 
+          modal === true 
+          ? <Modal  Detail={signcards}/>
+          : null
+      }
+
+        <div className="button-effect">
+        { signcards.map((a,i)=>{
+            return (
+        <button className="effect" type="button"  onClick={ ()=>{setmodal(!modal)}}
+        key={i}>{a.name} 
+        </button>
+        )})}
+        </div> 
+    </div>   
+
+              
+ 
+   </div>
+   )}
+
+function Modal(props){
+  return (
+
+    <div class='kan'> 
+        {/* <h4 className="pt-5">{props.signcards.name}</h4> */}
+          <p>수화설명</p>
+          <img src="" width="100%"/>
+          <p>영상</p>
+          <button className="btn btn-danger">단어장추가</button> 
+          <div>
+          <button className="btn btn-danger"> 이전 </button> 
+          <button className="btn btn-danger"> 목록 </button> 
+          <button className="btn btn-danger"> 다음  </button> 
+          </div>
+    </div>
+
+  )
+}
+
+
+export default SignPage;
