22b3e11a5e7b866944f1ca00c483bd6f8bbb999b
Juhwan Kim <juhwan.dev@gmail.com>
2022-02-09 23:13:50 +0900

    [S06P12D203-67] [AOS-김주환] refactor: 메인 페이지 UI 리뉴얼2

--- aos/Near/app/src/main/java/com/ssafy/near/src/main/home/HomeFragment.kt
+++ aos/Near/app/src/main/java/com/ssafy/near/src/main/home/HomeFragment.kt
@@ -1,79 +1,69 @@
 package com.ssafy.near.src.main.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.LinearLayout
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.DialogFragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.viewpager2.widget.ViewPager2
+import com.airbnb.lottie.LottieAnimationView
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.ssafy.near.R
 import com.ssafy.near.config.BaseFragment
 import com.ssafy.near.databinding.FragmentHomeBinding
 import java.util.*
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
-    lateinit var progressAdapter: ProgressAdapter
     private var timer = Timer()
     val introduceStr = arrayOf("당신의 곁에", "N:ear")
+
+    private val swipe: LottieAnimationView by lazy{requireActivity().findViewById(R.id.lottieView_swipe)}
+    private val bottomSheet: ConstraintLayout by lazy{requireActivity().findViewById(R.id.layout_bottom_sheet)}
+    lateinit var sheetBehavior: BottomSheetBehavior<ConstraintLayout>
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         initView()
+
+        sheetBehavior= BottomSheetBehavior.from(bottomSheet)
+        sheetBehavior.setBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                Log.d("테스트", "onSlide: 테스트")
+            }
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when(newState) {
+                    BottomSheetBehavior.STATE_COLLAPSED-> {
+                        swipe.visibility = View.VISIBLE
+                        binding.tvTypingSmallText.visibility = View.INVISIBLE
+                        binding.tvTypingLargeText.visibility = View.VISIBLE
+                    }
+                    BottomSheetBehavior.STATE_DRAGGING-> {
+                    }
+                    BottomSheetBehavior.STATE_EXPANDED-> {
+                        swipe.visibility = View.GONE
+                        binding.tvTypingSmallText.visibility = View.VISIBLE
+                        binding.tvTypingLargeText.visibility = View.INVISIBLE
+                    }
+                    BottomSheetBehavior.STATE_HIDDEN-> {
+                    }
+                    BottomSheetBehavior.STATE_SETTLING-> {
+                    }
+                }
+            }
+        })
+
+//        binding.tvTypingText.setOnClickListener {
+//            sheetBehavior.state=BottomSheetBehavior.STATE_EXPANDED
+//        }
+//        button_close.setOnClickListener {
+//            sheetBehavior.state=BottomSheetBehavior.STATE_HIDDEN
+//        }
     }
 
     private fun initView() {
-        /* 여백, 너비에 대한 정의 */
-        val pageMarginPx = resources.getDimensionPixelOffset(R.dimen.viewpager_page_margin) // dimens.xml 파일 안에 크기를 정의해두었다. (200dp)
-        val pagerWidth = resources.getDimensionPixelOffset(R.dimen.viewpage_page_width) // dimens.xml 파일이 없으면 생성해야함 (50dp)
-        val screenWidth = resources.displayMetrics.widthPixels // 스마트폰의 너비 길이를 가져옴
-        val offsetPx = screenWidth - pageMarginPx - pagerWidth
-
-//        binding.vpContent.also {
-//            it.offscreenPageLimit = 1
-//            it.adapter = ContentAdapter(mutableListOf("test", "test", "test", "test"))
-//            it.orientation = ViewPager2.ORIENTATION_HORIZONTAL
-//            it.setPageTransformer { page, position ->
-//                page.translationX = position * -offsetPx
-//            }
-//        }
-
-//        binding.rvProgress.apply {
-//            layoutManager = LinearLayoutManager(context)
-//            adapter = ProgressAdapter(mutableListOf("test", "test", "test", "test"))
-//        }
-
-//        var newStr = StringBuilder()
-//        var cnt = 0
-//        var index = 0
-//        var sequence = 0
-//
-//        while(true) {
-//            sequence = if((cnt / 2) % 2 == 0) {
-//                0
-//            } else {
-//                1
-//            }
-//
-//            var fullStr = introduceStr[sequence]
-//            when(cnt % 2) {
-//                0 -> {
-//                    newStr.append(fullStr[index++])
-//                    if(index == fullStr.length) {
-//                        cnt++
-//                        index--
-//                    }
-//                }
-//                1 -> {
-//                    newStr.deleteCharAt(index--)
-//                    if(index < 0) {
-//                        cnt++
-//                        index = 0
-//                    }
-//                }
-//            }
-//
-//            binding.tvTypingText.text = newStr.toString()
-//        }
-
         timer.schedule(object: TimerTask() {
             var newStr = StringBuilder()
             var cnt = 0
@@ -105,7 +95,8 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
                 }
 
                 requireActivity().runOnUiThread {
-                    binding.tvTypingText.text = newStr.toString()
+                    binding.tvTypingSmallText.text = newStr.toString()
+                    binding.tvTypingLargeText.text = newStr.toString()
                 }
             }
         }, 0, 300)
