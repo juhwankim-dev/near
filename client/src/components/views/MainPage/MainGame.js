4368551f54898af2ae7b06d81160964852cef301
김도연 <vlxjdus@gmail.com>
2022-02-13 08:34:51 +0000

    [INFRA - 김도연] fix : 경로수정

--- client/src/components/views/MainPage/MainGame.js
+++ client/src/components/views/MainPage/MainGame.js
@@ -18,7 +18,7 @@ const MainGame = () => {
 
   {/* ox퀴즈 위치 버튼 4k화면 사이즈 기준으로 맞춤 추후 다시 수정해야할듯*/}
     <div class="playleftbtn">
-      <a href="http://i6d203.p.ssafy.io:3000/oxquiz/start/cnt">
+      <a href="http://i6d203.p.ssafy.io/oxquiz/start/cnt">
     <span>play</span>
     </a>
     <span class="ok">play</span>
