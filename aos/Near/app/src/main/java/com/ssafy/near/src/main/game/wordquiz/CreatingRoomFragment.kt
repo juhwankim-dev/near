0622c61a98186af980493bbca286a76484b089b5
KimDoyeon <vlxjdus@gmail.com>
2022-02-14 17:00:44 +0900

    [S06P12D203-26][AOS - 김도연] fix: roomId가 아니라 roomInfo 전달로 수정

--- aos/Near/app/src/main/java/com/ssafy/near/src/main/game/wordquiz/CreatingRoomFragment.kt
+++ aos/Near/app/src/main/java/com/ssafy/near/src/main/game/wordquiz/CreatingRoomFragment.kt
@@ -1,12 +1,10 @@
 package com.ssafy.near.src.main.game.wordquiz
 
 import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.lifecycle.ViewModelProvider
 import com.ssafy.near.R
 import com.ssafy.near.config.BaseFragment
@@ -29,9 +27,7 @@ class CreatingRoomFragment : BaseFragment<FragmentCreatingRoomBinding>(R.layout.
             GameRepository())).get(WordQuizViewModel::class.java)
 
         wordQuizViewModel.getRoomInfo().observe(viewLifecycleOwner) {
-            val intent = Intent(requireActivity(), WordQuizActivity::class.java)
-            intent.putExtra("roomId", it.roomId)
-            startActivity(intent)
+            (context as RoomActivity).enterRoom(it)
         }
     }
 
