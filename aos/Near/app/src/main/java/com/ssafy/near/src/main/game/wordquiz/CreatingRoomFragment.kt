af700abeda58c0296c9e0475703289f9d72028f3
Juhwan Kim <juhwan.dev@gmail.com>
2022-02-14 20:59:21 +0900

    [S06P12D203-26][AOS-김주환] refactor: 퀴즈방 목록, 퀴즈방 생성 UI 리뉴얼

--- aos/Near/app/src/main/java/com/ssafy/near/src/main/game/wordquiz/CreatingRoomFragment.kt
+++ aos/Near/app/src/main/java/com/ssafy/near/src/main/game/wordquiz/CreatingRoomFragment.kt
@@ -1,8 +1,12 @@
 package com.ssafy.near.src.main.game.wordquiz
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.lifecycle.ViewModelProvider
 import com.ssafy.near.R
 import com.ssafy.near.config.BaseFragment
@@ -17,13 +21,7 @@ class CreatingRoomFragment : BaseFragment<FragmentCreatingRoomBinding>(R.layout.
         super.onViewCreated(view, savedInstanceState)
 
         initVieModel()
-
-        binding.btnSave.setOnClickListener {
-            val roomName = binding.etRoomName.text.toString().trim()
-            if (roomName != "") {
-                wordQuizViewModel.createRoom(roomName)
-            }
-        }
+        initEvent()
     }
 
     private fun initVieModel() {
@@ -35,5 +33,28 @@ class CreatingRoomFragment : BaseFragment<FragmentCreatingRoomBinding>(R.layout.
             intent.putExtra("roomId", it.roomId)
             startActivity(intent)
         }
+    }
+
+    private fun initEvent() {
+        binding.btnSave.setOnClickListener {
+            val roomName = binding.etRoomName.text.toString().trim()
+            if (roomName != "") {
+                wordQuizViewModel.createRoom(roomName)
+            }
+        }
+
+        binding.ivChange.setOnClickListener {
+            binding.etRoomName.setFocusAndShowKeyboard(requireContext())
+        }
+    }
+
+    fun EditText.setFocusAndShowKeyboard(context: Context) {
+        this.requestFocus()
+        setSelection(this.text.length)
+        this.postDelayed({
+            val inputMethodManager =
+                context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.showSoftInput(this, InputMethodManager.SHOW_FORCED)
+        }, 100)
     }
 }
