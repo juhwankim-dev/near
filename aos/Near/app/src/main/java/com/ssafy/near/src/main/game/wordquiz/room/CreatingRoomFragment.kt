c9d6f2de6681c2c5ed2d292def079a897b376d20
KimDoyeon <vlxjdus@gmail.com>
2022-02-15 15:10:52 +0900

    [S06P12D203-49][AOS - 김도연] fix : rebase 후 빠진 파일들 추가.

---
+++ aos/Near/app/src/main/java/com/ssafy/near/src/main/game/wordquiz/room/CreatingRoomFragment.kt
@@ -0,0 +1,58 @@
+package com.ssafy.near.src.main.game.wordquiz.room
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import androidx.lifecycle.ViewModelProvider
+import com.ssafy.near.R
+import com.ssafy.near.config.BaseFragment
+import com.ssafy.near.databinding.FragmentCreatingRoomBinding
+import com.ssafy.near.repository.GameRepository
+import com.ssafy.near.src.main.game.wordquiz.WordQuizViewModel
+import com.ssafy.near.src.main.game.wordquiz.WordQuizViewModelFactory
+
+
+class CreatingRoomFragment : BaseFragment<FragmentCreatingRoomBinding>(R.layout.fragment_creating_room) {
+    private lateinit var wordQuizViewModel: WordQuizViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initVieModel()
+        initEvent()
+    }
+
+    private fun initVieModel() {
+        wordQuizViewModel = ViewModelProvider(requireActivity(), WordQuizViewModelFactory(
+            GameRepository())).get(WordQuizViewModel::class.java)
+
+        wordQuizViewModel.getRoomInfo().observe(viewLifecycleOwner) {
+            (context as RoomActivity).enterRoom(it)
+        }
+    }
+
+    private fun initEvent() {
+        binding.btnSave.setOnClickListener {
+            val roomName = binding.etRoomName.text.toString().trim()
+            if (roomName != "") {
+                wordQuizViewModel.createRoom(roomName)
+            }
+        }
+
+        binding.ivChange.setOnClickListener {
+            binding.etRoomName.setFocusAndShowKeyboard(requireContext())
+        }
+    }
+
+    fun EditText.setFocusAndShowKeyboard(context: Context) {
+        this.requestFocus()
+        setSelection(this.text.length)
+        this.postDelayed({
+            val inputMethodManager =
+                context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.showSoftInput(this, InputMethodManager.SHOW_FORCED)
+        }, 100)
+    }
+}
---
+++ aos/Near/app/src/main/java/com/ssafy/near/src/main/game/wordquiz/room/RoomListAdapter.kt
@@ -0,0 +1,43 @@
+package com.ssafy.near.src.main.game.wordquiz.room
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ssafy.near.databinding.ListItemGameRoomBinding
+import com.ssafy.near.dto.RoomInfo
+
+class RoomListAdapter : RecyclerView.Adapter<RoomListAdapter.RoomInfoHolder>() {
+    interface ItemClickListener {
+        fun onClick(roomInfo: RoomInfo)
+    }
+
+    private lateinit var itemClickListener: ItemClickListener
+    var roomList = mutableListOf<RoomInfo>()
+
+    inner class RoomInfoHolder(private val binding: ListItemGameRoomBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bindInfo(roomInfo: RoomInfo) {
+            binding.rooomInfo = roomInfo
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RoomInfoHolder {
+        val listItemBinding = ListItemGameRoomBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return RoomInfoHolder(listItemBinding)
+    }
+
+    override fun onBindViewHolder(holder: RoomInfoHolder, position: Int) {
+        holder.apply {
+            bindInfo(roomList[position])
+
+            itemView.setOnClickListener {
+                itemClickListener.onClick(roomList[position])
+            }
+        }
+    }
+
+    override fun getItemCount(): Int = roomList.size
+
+    fun setItemClickListener(itemClickListener: ItemClickListener) {
+        this.itemClickListener = itemClickListener
+    }
+}
