569b159733e43bfbee5fe9f958814dd8717a3730
wooyeongs2 <wooyeongs2@gmail.com>
2022-02-17 12:19:06 +0900

    [S06P12D203-56] [FE-고우영] fix: 머지 충돌 시 발생한 코드 수정

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -8,16 +8,12 @@ function NavBar(props) {
   return(
     <div>
     <Navbar bg="light" expand="lg" >
-    <Container >   
-    <Navbar.Brand as={Link} to='/main' style={{fontSize:'2.5vh'}}> </Navbar.Brand>
     <Container>   
+    <Navbar.Brand as={Link} to='/main' style={{fontSize:'2.5vh'}}> </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav" style={{  flexDirection:'column',alignContent:'flex-end',flexWrap: 'wrap'}}>
       <Nav className="me-auto">
-        {/* <Nav.Link href="#home">Home</Nav.Link> */}
-        {/* <Nav.Link href="login">Link</Nav.Link> */}
         <NavDropdown style={{fontSize:'1.5vh', fontWeight:'bold', marginRight:'5px',  }} title="학습컨텐츠" id="basic-nav-dropdown">
-        <NavDropdown title="학습컨텐츠" id="basic-nav-dropdown">
           <NavDropdown.Item as={Link} to='/finger/1'>지어</NavDropdown.Item>
           <NavDropdown.Item as={Link} to='/sign'>수어</NavDropdown.Item>
         </NavDropdown>
@@ -26,13 +22,9 @@ function NavBar(props) {
           <NavDropdown.Item as={Link} to='/fingerquiz'>지문자퀴즈</NavDropdown.Item>
         </NavDropdown>
         <Nav.Link style={{fontSize:'1.5vh', fontWeight:'bold', marginRight:'5px',  }} as={Link} to='/myvoca'>단어장</Nav.Link>
-        
 
-        
         {/* 로그인 상태 */}
         <NavDropdown style={{fontSize:'1.5vh', fontWeight:'bold', marginRight:'5px',  }} title="마이페이지" id="basic-nav-dropdown">
-          <NavDropdown.Item href="/mystudy">나의학습</NavDropdown.Item>
-        <NavDropdown title="마이페이지" id="basic-nav-dropdown">
           <NavDropdown.Item as={Link} to='/login'>로그인</NavDropdown.Item>
           <NavDropdown.Item as={Link} to='/register'>회원가입</NavDropdown.Item>
           <NavDropdown.Divider />
@@ -42,13 +34,6 @@ function NavBar(props) {
     </Navbar.Collapse>
     </Container>
     </Navbar>  
-
-    {/* <Switch>
-    <Route path='/login' component={Login}>
-    </Route>
-    <Route path='/main' component={Mainpage}>
-    </Route>
-    </Switch> */}
     </div>
   )
 }
