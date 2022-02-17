9c7ca43518b6a9a894e486333a9adeaa0620e5c7
elenaisnanocat <elenassafy0201@gmail.com>
2022-02-17 17:10:36 +0900

    fix: register link change

--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -72,7 +72,7 @@ function RegisterPage(props) {
    const idCheck = () => {
     axios({
       method: 'get',
-      url: `https://hoonycode2.loca.lt/api/sign/checkid/?uid=${Id}`,
+      url: `http://i6d203.p.ssafy.io:8185/api/sign/checkid/?uid=${Id}`,
     })
       .then((res) => {
         console.log(res.data);
@@ -92,7 +92,7 @@ function RegisterPage(props) {
    const emailCheck = () => {
     axios({
       method: 'get',
-      url: `https://hoonycode2.loca.lt/api/sign/checkemail?email=${Email}`,
+      url: `http://i6d203.p.ssafy.io:8185/api/sign/checkemail?email=${Email}`,
     })
     .then((res) => {
       console.log(res.data);
@@ -112,7 +112,7 @@ function RegisterPage(props) {
   const nicknameCheck = () => {
     axios({
       method: 'get',
-      url: `https://hoonycode2.loca.lt/api/sign/nickname?nickname=${Nickname}`,
+      url: `http://i6d203.p.ssafy.io:8185/api/sign/nickname?nickname=${Nickname}`,
     })
     .then((res) => {
       console.log(res.data);
