23f4fedb60577716c8ea40f1fcc06935464fed50
wooyeongs2 <wooyeongs2@gmail.com>
2022-02-17 13:24:57 +0900

    [S06P12D203] [FE-고우영] feat: 회원가입 및 로그인 창 UI 수정 및 중복검사 기능 구현

--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -5,6 +5,7 @@ import { registerUser } from '../../../_actions/user_action';
 import toast, { Toaster } from 'react-hot-toast';
 import '../LoginPage/Accounts.css';
 import axios from 'axios';
+import {Button } from 'react-bootstrap';
 
 function RegisterPage(props) {
   let navigate = useNavigate();
@@ -66,24 +67,66 @@ function RegisterPage(props) {
   };
 
 
-   // 이메일 중복 체크
-   const emailCheck = () => {
+
+   // 아이디 중복 체크
+   const idCheck = () => {
     axios({
       method: 'get',
-      url: `https://hoonycode.loca.lt/api/sign/checkemail/${Email}`,
+      url: `https://hoonycode2.loca.lt/api/sign/checkid/?uid=${Id}`,
     })
       .then((res) => {
-        if (res.data.isDuple) {
+        console.log(res.data);
+        if (res.data.data === false) {
+          toast.success('사용할 수 있는 아이디입니다.')
           ;
         } else {
-          toast.error('이미 가입한 메일입니다.');
+          toast.error('이미 사용중인 아이디 입니다.');
         }
       })
       .catch((err) => {
         console.log(err);
       });
   };
+
+   // 이메일 중복 체크
+   const emailCheck = () => {
+    axios({
+      method: 'get',
+      url: `https://hoonycode2.loca.lt/api/sign/checkemail?email=${Email}`,
+    })
+    .then((res) => {
+      console.log(res.data);
+      if (res.data.data === false) {
+        toast.success('사용할 수 있는 이메일입니다.')
+        ;
+      } else {
+        toast.error('이미 사용중인 이메일 입니다.');
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  };
   
+  // 닉네임 중복 체크
+  const nicknameCheck = () => {
+    axios({
+      method: 'get',
+      url: `https://hoonycode2.loca.lt/api/sign/nickname?nickname=${Nickname}`,
+    })
+    .then((res) => {
+      console.log(res.data);
+      if (res.data.data === false) {
+        toast.success('사용할 수 있는 닉네임입니다.')
+        ;
+      } else {
+        toast.error('이미 사용중인 닉네임 입니다.');
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  };
 
   const onSubmit = (e) => {
 		e.preventDefault();
@@ -98,7 +141,7 @@ function RegisterPage(props) {
 
 		if (Password === ConfirmPasword) {
 
-      axios.post('https://hoonycode.loca.lt/api/sign/signup', 
+      axios.post('https://hoonycode2.loca.lt/api/sign/signup', 
       JSON.stringify({  
         email: Email,
         uid: Id,
@@ -160,15 +203,16 @@ function RegisterPage(props) {
   <div>
   <form style={{
     display:'flex', justifyContent: 'center', alignItems: 'center'
-    ,width: '100%', height: '100vh'}}
+    ,width: '100%', height: '95vh'}}
         onSubmit={onSubmitHandler}>
-  <div class="login-wrap">
-	<div class="login-html">
-    <div><h1 className='headerst'>N:ear</h1></div>
+  <div style={{ height:'70vh', width:'160vh',  }}  class="login-wrap">
     
-		<input id="tab-1" type="radio" name="tab" class="sign-in" onClick={()=>{ navigate('/login')}} /><label for="tab-1" class="tab">Sign In</label>
+	<div style={{marginTop:'0px',  marginBottom:'0px', paddingTop:'80px', paddingBottom:'0px'}} class="login-html">
+    <div style={{fontWeight:'bold' , marginTop:'0px', marginBottom:'0px', paddingTop:'0px', paddingBottom:'0px'}}><h1 className='headerst'>N:ear</h1></div>
+    
+		<input  style={{marginTop:'0px',  marginBottom:'0px', paddingTop:'0px', paddingBottom:'0px'}} id="tab-1" type="radio" name="tab" class="sign-in" onClick={()=>{ navigate('/login')}} /><label for="tab-1" class="tab">Sign In</label>
     {/* SIGN IN버튼 누를 경우 login페이지로 랜더링되게 변경 */}
-		<input id="tab-2" type="radio" name="tab" class="sign-up" checked /><label for="tab-2" class="tab">Sign Up</label>
+		<input style={{marginTop:'0px',  marginBottom:'0px', paddingTop:'0px', paddingBottom:'0px'}} id="tab-2" type="radio" name="tab" class="sign-up" checked /><label for="tab-2" class="tab">Sign Up</label>
 	 
 
      <div class="login-form">
@@ -176,30 +220,35 @@ function RegisterPage(props) {
         <div class="sign-up-htm">
           <div class="group">
             <label for="user" class="label">ID</label>
-            <input id="user" type="text" class="input" placeholder="6~20자 내로 입력해주세요." onChange={onIdHandler}/>
-          </div>
-          <div class="group">
-            <label for="pass" class="label">Password</label>
-            <input id="pass" type="password" class="input" data-type="password" placeholder="문자, 숫자, 기호를 조합하여 8~20자 내로 입력해주세요."  onChange={onPasswordHanlder}/>
-          </div>
-          <div class="group">
-            <label for="pass" class="label">Repeat Password</label>
-            <input id="pass" type="password" class="input" data-type="password" placeholder="비밀번호를 확인해주세요" onChange={onConfirmPasswordHandler}/>
-          </div>
+            <input style={{ color:'black'}}  id="user" type="text" class="input" placeholder="6~20자 내로 입력해주세요." onChange={onIdHandler}/>
+            {/* <Button style={{ marginTop:'8px' , marginLeft:'300px' }} >중복체크</Button> */}
+            <p style={{ marginTop:'8px' , marginLeft:'400px', fontSize:'17px', cursor:'pointer' }} onClick={()=>{idCheck()}} >중복 체크</p>
+            </div>
           <div class="group">
             <label for="pass" class="label">Nickname</label>
-            <input id="pass" type="text" class="input" placeholder="1~6자 내로 입력해주세요." onChange={onNicknameHanlder}/>
-          </div>
+            <input style={{ color:'black'}} id="pass" type="text" class="input" placeholder="1~6자 내로 입력해주세요." onChange={onNicknameHanlder}/>
+            <p style={{ marginTop:'8px' , marginLeft:'400px', fontSize:'17px', cursor:'pointer' }} onClick={()=>{nicknameCheck()}} >중복 체크</p>
+          </div> 
           <div class="group">
             <label for="pass" class="label">Email Address</label>
-            <input id="pass" type="email" class="input" placeholder="메일주소를 입력해주세요." onChange={onEmailHandler}/>
+            <input style={{ color:'black'}} id="pass" type="email" class="input" placeholder="메일주소를 입력해주세요." onChange={onEmailHandler}/>
+            <p style={{ marginTop:'8px' , marginLeft:'400px', fontSize:'17px', cursor:'pointer',  }} onClick={()=>{emailCheck()}} >중복 체크</p>
+          </div>  
+          <div style={{  marginBottom:'35px', marginTop:'0px' }} class="group">
+            <label for="pass" class="label">Password</label>
+            <input style={{ color:'black', }} id="pass" type="password" class="input" data-type="password" placeholder="문자, 숫자, 기호를 조합하여 8~20자 내로 입력해주세요."  onChange={onPasswordHanlder}/>
           </div>
-          <div class="group">
-            <input type="submit" class="button" value="Sign Up" />
+          <div  style={{  marginBottom:'55px' }} class="group">
+            <label for="pass" class="label">Repeat Password</label>
+            <input style={{ color:'black'}} id="pass" type="password" class="input" data-type="password" placeholder="비밀번호를 확인해주세요" onChange={onConfirmPasswordHandler}/>
           </div>
-          <div class="hr"></div>
+        
+          <div  class="group">
+            <input style={{ fontSize:'21px' }} type="submit" class="button" value="Sign Up" />
+          </div>
+          <div  style={{ marginTop: '30px'}}class="hr"></div>
           <div class="foot-lnk">
-            <label for="tab-1" onClick={()=>{ navigate('/login')}} >Already Member?</label>
+            <label style={{ fontSize: '20px' }} for="tab-1" onClick={()=>{ navigate('/login')}} >Already Member?</label>
           {/* Already Member? 버튼 누를 경우 login페이지로 랜더링되게 변경 */}
           </div>
         </div>
