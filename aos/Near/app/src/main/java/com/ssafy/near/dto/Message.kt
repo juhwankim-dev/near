ffba70e1d19a040fb771e02ab621381157639d67
Juhwan Kim <juhwan.dev@gmail.com>
2022-02-23 23:08:24 +0900

    [AOS-김주환] feat: 멀티플레이 채팅 기능 구현

--- aos/Near/app/src/main/java/com/ssafy/near/dto/Message.kt
+++ aos/Near/app/src/main/java/com/ssafy/near/dto/Message.kt
@@ -2,8 +2,8 @@ package com.ssafy.near.dto
 
 import java.io.Serializable
 
-data class Message(val type: MsgType, val roomId: String, val sender: String, val message: String) : Serializable
+data class Message(var type: MsgType, val roomId: String, val sender: String, val message: String) : Serializable
 
 enum class MsgType {
-    ENTER, OUT, TALK, START, END, CHANGE
+    ENTER, OUT, TALK, START, END, CHANGE, ANSWER, NOTICE
 }
